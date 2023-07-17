973b88a56327909c31e1e84121ab2bf21ebaf8de
DavidSandovalInacap <138266985+DavidSandovalInacap@users.noreply.github.com>
2023-07-16 20:23:54 -0400

    mover_producto.py

--- ElGranPoeta/mover_producto.py
+++ ElGranPoeta/mover_producto.py
@@ -9,59 +9,46 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-
-def mostrar_menu():
-    print("--- MOVIMIENTO DE PRODUCTOS ---")
-    print("1. Agregar productos a una bodega")
-    print("2. Sacar productos de una bodega")
-    print("3. Generar documento de movimiento")
-    print("4. Salir")
-
-
-def agregar_productos():
+def mover_productos():
     id_bodega_origen = int(input("ID de la bodega de origen: "))
     id_bodega_destino = int(input("ID de la bodega de destino: "))
     id_producto = int(input("ID del producto: "))
-    cantidad = int(input("Cantidad a agregar: "))
+    cantidad_a_mover = int(input("Cantidad a mover: "))
     id_empleado = int(input("ID del empleado que realiza el movimiento: "))
 
     try:
-        query = "UPDATE stock SET cantidad_stock = cantidad_stock + %s WHERE id_bodega = %s AND id_producto = %s"
-        values = (cantidad, id_bodega_destino, id_producto)
-        cursor.execute(query, values)
+        # Verificar si hay suficiente stock en la bodega de origen
+        query_stock_origen = "SELECT cantidad_stock FROM stock WHERE id_bodega = %s AND id_producto = %s"
+        values_stock_origen = (id_bodega_origen, id_producto)
+        cursor.execute(query_stock_origen, values_stock_origen)
+        stock_origen = cursor.fetchone()
+
+        if not stock_origen or stock_origen[0] < cantidad_a_mover:
+            print("No hay suficiente stock en la bodega de origen.")
+            return
+
+        # Actualizar el stock en la bodega de origen
+        query_update_origen = "UPDATE stock SET cantidad_stock = cantidad_stock - %s WHERE id_bodega = %s AND id_producto = %s"
+        values_update_origen = (cantidad_a_mover, id_bodega_origen, id_producto)
+        cursor.execute(query_update_origen, values_update_origen)
         db.commit()
-        print("Productos agregados exitosamente a la bodega de destino.")
+        print("Productos movidos exitosamente desde la bodega de origen.")
 
-        query = "INSERT INTO movimiento (fecha_movimiento, id_bodega_origen, id_bodega_destino, id_empleado, id_detalle) VALUES (CURDATE(), %s, %s, %s, %s)"
-        values = (id_bodega_origen, id_bodega_destino, id_empleado, id_producto)
-        cursor.execute(query, values)
+        # Actualizar el stock en la bodega de destino
+        query_update_destino = "UPDATE stock SET cantidad_stock = cantidad_stock + %s WHERE id_bodega = %s AND id_producto = %s"
+        values_update_destino = (cantidad_a_mover, id_bodega_destino, id_producto)
+        cursor.execute(query_update_destino, values_update_destino)
         db.commit()
-        print("Documento de movimiento generado exitosamente.")
-    except mysql.connector.Error as error:
-        print("Error al agregar los productos o generar el documento de movimiento:", error)
+        print("Productos movidos exitosamente a la bodega de destino.")
 
-
-def sacar_productos():
-    id_bodega = int(input("ID de la bodega: "))
-    id_producto = int(input("ID del producto: "))
-    cantidad = int(input("Cantidad a sacar: "))
-    id_empleado = int(input("ID del empleado que realiza el movimiento: "))
-
-    try:
-        query = "UPDATE stock SET cantidad_stock = cantidad_stock - %s WHERE id_bodega = %s AND id_producto = %s"
-        values = (cantidad, id_bodega, id_producto)
-        cursor.execute(query, values)
-        db.commit()
-        print("Productos sacados exitosamente de la bodega.")
-
-        query = "INSERT INTO movimiento (fecha_movimiento, id_bodega_origen, id_bodega_destino, id_empleado, id_detalle) VALUES (CURDATE(), %s, %s, %s, %s)"
-        values = (id_bodega, id_bodega, id_empleado, id_producto)
-        cursor.execute(query, values)
+        # Insertar el registro de movimiento
+        query_insert_movimiento = "INSERT INTO movimiento (fecha_movimiento, id_bodega_origen, id_bodega_destino, id_empleado, id_detalle) VALUES (CURDATE(), %s, %s, %s, %s)"
+        values_insert_movimiento = (id_bodega_origen, id_bodega_destino, id_empleado, id_producto)
+        cursor.execute(query_insert_movimiento, values_insert_movimiento)
         db.commit()
         print("Documento de movimiento generado exitosamente.")
     except mysql.connector.Error as error:
-        print("Error al sacar los productos o generar el documento de movimiento:", error)
-
+        print("Error al mover los productos o generar el documento de movimiento:", error)
 
 def generar_documento_movimiento():
     try:
@@ -84,23 +71,23 @@ def generar_documento_movimiento():
     except mysql.connector.Error as error:
         print("Error al generar el documento de movimiento:", error)
 
+def mostrar_menu():
+    while True:
+        print("--- MOVIMIENTO DE PRODUCTOS ---")
+        print("1. Mover productos entre bodegas")
+        print("2. Generar documento de movimiento")
+        print("3. Salir")
 
+        opcion = input("Ingrese una opción: ")
+        if opcion == "1":
+            mover_productos()
+        elif opcion == "2":
+            generar_documento_movimiento()
+        elif opcion == "3":
+            break
+        else:
+            print("Opción inválida. Intente nuevamente.")
 
-while True:
-    mostrar_menu()
-    opcion = input("Ingrese una opción: ")
-
-    if opcion == "1":
-        agregar_productos()
-    elif opcion == "2":
-        sacar_productos()
-    elif opcion == "3":
-        generar_documento_movimiento()
-    elif opcion == "4":
-        break
-    else:
-        print("Opción inválida. Intente nuevamente.")
-
-
+mostrar_menu()
 cursor.close()
 db.close()
